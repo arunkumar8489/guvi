896259250426c8c98ef2f84f6e3d4cd2991c0f72
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-06-04 16:34:33 +0530

    Create code2.py

---
+++ python/code2.py
@@ -0,0 +1,9 @@
+jk=input()
+if((jk>='a' and jk<='z')or(jk>='A' and jk<='Z')):
+    if(jk=='a' or jk=='A' or jk=='e' or jk=='E' or jk=='i' or jk=='I' or jk=='o' or jk=='O' or jk=='u' or jk=='U'):
+        print("Vowel")
+    else:
+        print("consonant")
+else:
+    print("invalid")        
+        
