3342f31735e1500f65d511b69dfb256fa8aca45c
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-06-04 16:38:00 +0530

    Update code2.py

--- python/code2.py
+++ python/code2.py
@@ -3,7 +3,7 @@ if((jk>='a' and jk<='z')or(jk>='A' and jk<='Z')):
     if(jk=='a' or jk=='A' or jk=='e' or jk=='E' or jk=='i' or jk=='I' or jk=='o' or jk=='O' or jk=='u' or jk=='U'):
         print("Vowel")
     else:
-        print("consonant")
+        print("Consonant")
 else:
     print("invalid")        
         
