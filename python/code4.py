6ea7843f8e721737de70389b7292c0656c9f49ea
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-06-05 22:19:43 +0530

    Update code4.py

--- python/code4.py
+++ python/code4.py
@@ -1,12 +1,12 @@
-n1 = float(input("Enter first number: "))
-n2 = float(input("Enter second number: "))
-n3 = float(input("Enter third number: "))
+jk = float(input("Enter first number: "))
+kl = float(input("Enter second number: "))
+lp = float(input("Enter third number: "))
  
-if (n1 > n2) and (n1 > n3):
-   largest = n1
-elif (n2 > n1) and (n2 > n3):
-   largest = n2
+if (jk > kl) and (jk > lp):
+   largest = jk
+elif (kl > lp) and (kl > lp):
+   largest = kl
 else:
-   largest = n3
+   largest = lp
  
 print("The largest number is",largest)
