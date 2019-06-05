fc8922ab439bd86b4750188c2efa791c6a1aa5d1
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-06-05 22:04:55 +0530

    Update code4.py

--- python/code4.py
+++ python/code4.py
@@ -1,6 +1,2 @@
-list1 = []  
-num = int(input("Enter number of elements in list: "))  
-for i in range(1, num + 1): 
-    ele = int(input("Enter elements: ")) 
-    list1.append(ele)  
-print("Largest element is:", max(list1)) 
+list1 = [3,4,5]    
+print(max(list1))
