ff530f45399b68acfc9cbfa44642d2490ab16a34
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-06-10 21:32:01 +0530

    Update code2.12.py

--- python/code2.12.py
+++ python/code2.12.py
@@ -1,11 +1,11 @@
-n=int(input())
-temp=n
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):
+nn=int(input())
+temp=nn
+reve=0
+while(nn>0):
+    dig=nn%10
+    reve=reve*10+dig
+    nn=nn//10
+if(temp==reve):
     print("yes")
 else:
     print("no")
