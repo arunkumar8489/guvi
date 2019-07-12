dfe52a10b8f41e15b3caeaf3c798edc51f991a64
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-07-12 22:46:44 +0530

    Update hu1.py

--- python/hu1.py
+++ python/hu1.py
@@ -1,14 +1,17 @@
 n=int(input())
-l=[]
-v=0
-for i in range(0,n):
-    m=int(input())
-    l.append(m)
-for j in range(0,len(l)):
-    for b in range(0,j):
-        if l[j]==l[b]:
-            print(l[j],end=' ')
-            v+=1
-    l.sort()
-if v==0:
+m=input()
+o=m.split(" ")
+p=[]
+q=[]
+t=0
+for i in o:
+    p.append(int(i))
+for j in range(0,len(p)):
+    for k in range(j+1,len(p)):
+        if p[j]==p[k] and not(p[j] in q):
+            q.append(p[j])
+q.sort()
+for t in q:
+    print(l,end=" ")
+if t==0:
     print('unique')
