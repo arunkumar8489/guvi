d6a62d9958cb9136a6657452384b3fefcb88d46b
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-07-13 08:05:43 +0530

    Create pla21.py

---
+++ python/pla21.py
@@ -0,0 +1,7 @@
+a,s=map(int,input().split(" "))
+r,d=map(int,input().split(" "))
+f,b=map(int,input().split(" "))
+if((a==r==f) or (a==s) or (r==d) or (f==b)):
+     print("yes")
+else:
+     print("no")
