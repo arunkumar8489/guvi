30f56fb90216cd51462aa6d2a52cf599ad2d7409
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-07-17 21:55:26 +0530

    Create 52.py

---
+++ python/52.py
@@ -0,0 +1,22 @@
+n=int(input())
+if n==1:
+    word="One"
+if n==2:
+    word="Two"
+if n==3:
+    word="Three"
+if n==4:
+    word="Four"
+if n==5:
+    word="Five"
+if n==6:
+    word="Six"
+if n==7:
+    word="Seven"
+if n==8:
+    word="Eight"
+if n==9:
+    word="Nine"
+if n==10:
+    word="Ten"    
+print(word)    
