e2833f508d0c2e44f7967b2f76b81a7122bde434
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-07-28 22:58:09 +0530

    Update pro53.py

--- python/pro53.py
+++ python/pro53.py
@@ -1,11 +1,10 @@
-def nu(n):
-    a="abcdefghijklmnopqrstuvwxyz"
-    for i in a:
-        if i not in n.lower():
-            return False
-        return True
-n=str(input().split())
-if(nu(n)==True):
-    print('yes')
-else:
-    print('no')
+def nu(n): 
+      n= n.lower()
+      n= set(n)
+      a = [ ch for ch in n if ord(ch) in range(ord('a'), ord('z')+1)]
+      if len(a) == 26: 
+        return 'yes'
+      else: 
+        return 'no'
+n=input()
+print(nu(n)) 
