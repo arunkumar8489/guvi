626ede3435434b27752c2cd17e340d436622b8f0
arunkumar8489 <51322903+arunkumar8489@users.noreply.github.com>
2019-07-29 21:05:32 +0530

    Update pro57.py

--- python/pro57.py
+++ python/pro57.py
@@ -1,5 +1,10 @@
-n,m=input().split()
-if(n.find(m)==-1):
-      print("no")
+n,m=map(str,input().split())
+c=0
+for i in range(0,len(n)):
+    for j in range(0,len(m)):
+        if n[i]==m[j]:
+            c+=1
+if c>=2:
+    print("yes")
 else:
-      print("yes")
+    print("no")
